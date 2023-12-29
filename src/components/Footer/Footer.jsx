cd7dc57823dc52e528221b297a537560ea90efea
Chetan Sharma <66529151+chetan-sharma-g@users.noreply.github.com>
2023-12-29 21:19:20 +0530

    Home page responsiveness done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 
 import { Link } from "react-router-dom";
 import { FaInstagram, FaFacebook } from "react-icons/fa";
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,9 +9,9 @@ const Home = () => {
     <>
       <div className="h-[37rem] sm:h-screen flex flex-col sm:flex-row">
         {/* Left Section */}
-        <div className="h-2/4 sm:h-full flex flex-col justify-center p-10 sm:px-20 bg-slate-500 rounded-br-3xl">
+        <div className="h-2/4 sm:h-full flex flex-col justify-center p-10 sm:px-20 bg-slate-500 sm:rounded-br-3xl">
           <h1 className="text-4xl sm:text-6xl text-black">
-            DR.<span className="text-[#60cf80] font-bold">Devesh</span>Jangid
+            DR.<span className="text-[#60cf80] font-bold">Devesh</span>Sharma
           </h1>
           <h2 className="text-xl sm:text-3xl text-black">
             A <span className="text-[#60cf80] font-bold">HomeoPath</span> who
@@ -37,50 +37,50 @@ const Home = () => {
       </div>
 
       {/* Hero Section: Why Homeopathy */}
-      <div className="min-h-screen bg-gray-100 py-10 sm:py-32 px-5 sm:px-[15rem] mx-auto">
-        <h3 className="text-2xl sm:text-4xl text-black font-bold mb-5">
+      <div className=" min-h-screen min-w-screen bg-gray-100 py-10 sm:py-32 px-5 sm:px-[15rem]">
+        <h3 className="min-w-screen text-2xl sm:text-4xl text-black font-bold mb-5 p-5 text-center bg-[#60cf80]">
           Why Choose Homeopathy?
         </h3>
-        <ul className="list-none px-32">
-          <li className="text-lg sm:text-xl text-black mb-3 flex flex-row gap-5 p-5">
+        <ul className="flex flex-col flex-wrap list-none sm:px-32">
+          <li className="text-lg sm:text-xl text-black mb-3 sm:flex sm:flex-row sm:gap-5 p-5">
             <span>
               <img src={sweet} alt="" className="h-52 rounded-lg border-2 border-spacing-5 border-black" />
             </span>
-            <div className="w-2/5">
-              <p className="p-10 text-center text-2xl">
+            <div className="sm:w-2/5">
+              <p className="p-5 sm:p-10 text-center text-2xl">
               Homeopathy relies on the principle of like cures like and highly
               diluted substances
             </p>
             </div>
           </li>
-          <li className="text-lg sm:text-xl text-black mb-3 flex flex-row-reverse  gap-5 p-5">
+          <li className="text-lg sm:text-xl text-black mb-3 sm:flex sm:flex-row-reverse  sm:gap-5 p-5">
             <span>
               <img src={homo} alt="" className="h-52 rounded-lg border-2 border-spacing-5 border-black" />
             </span>
-            <div className="w-3/4">
-              <p className="p-5 text-center text-2xl">
+            <div className="sm:w-3/4">
+              <p className="p-5 sm:p-10 text-center text-2xl  ">
               Allopathy (conventional medicine) is evidence-based and undergoes
             rigorous scientific testing.
             </p>
             </div>
           </li>
-          <li className="text-lg sm:text-xl text-black mb-3 flex flex-row gap-5 p-5">
+          <li className="text-lg sm:text-xl text-black mb-3 sm:flex sm:flex-row sm:gap-5 p-5">
             <span>
               <img src={homo2} alt="" className="h-52 rounded-lg border-2 border-spacing-5 border-black" />
             </span>
-            <div className="w-2/5">
-              <p className="p-10 text-center text-2xl">
+            <div className="sm:w-2/5">
+              <p className="p-5 sm:p-10 text-center text-2xl  ">
               Homeopathy lacks substantial scientific evidence supporting its
             efficacy.
             </p>
             </div>
           </li>
-          <li className="text-lg sm:text-xl text-black mb-3 flex flex-row-reverse  gap-5 p-5">
+          <li className="text-lg sm:text-xl text-black mb-3 sm:flex sm:flex-row-reverse sm:gap-5 p-5">
             <span>
               <img src={homo3} alt="" className="h-52 rounded-lg border-2 border-spacing-5 border-black" />
             </span>
-            <div className="w-3/4">
-              <p className="p-5 text-center text-2xl">
+            <div className="sm:w-3/4">
+              <p className="p-5 sm:p-10 text-center text-2xl  ">
               Allopathy treatments are often backed by extensive research and
             clinical trials.
             </p>
