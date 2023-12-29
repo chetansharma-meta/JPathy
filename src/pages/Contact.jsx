2ff5cd953741360e58f7a3667f80d812328b4d0f
Chetan Sharma <66529151+chetan-sharma-g@users.noreply.github.com>
2023-12-29 14:09:58 +0530

    Added some more sections and functionalities in website

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+
 
 const Contact = () => {
   return (
     <div>
-      Contact
+      <div className="min-h-screen ">
+        <div className="flex flex-col p-5 sm:p-10">
+          <div className="w-full text-black">
+            <h1 className="text-center text-4xl md:text-5xl font-bold">
+              Google Map Location
+            </h1>
+          </div>
+          <div className="flex justify-center">
+            <div className="w-full md:w-3/4 lg:w-1/2">
+              <div className="aspect-w-16 aspect-h-9 pt-20">
+                <iframe
+                  title="Google Map"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d221.32420876362528!2d77.6997967338696!3d27.432271713879487!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39737702797e9af5%3A0x3cf85c1c0ac2996e!2sCMJX%2BWVV%2C%20Narsipuram%20Colony%2C%20Mathura%2C%20Uttar%20Pradesh%20281006!5e0!3m2!1sen!2sin!4v1703830076806!5m2!1sen!2sin"
+                  style={{ border: "0" }}
+                  allowFullScreen=""
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
+                  className="rounded-lg md:rounded-xl lg:rounded-2xl w-full h-96 md:h-80 lg:h-96"
+                ></iframe>
+                </div>
+            </div>
+          </div>
+        </div>
+          <div className="bg-gray-200 p-10 text-center font-bold text-xl mt-12">
+          <p className="p-5 text-5xl">Jangid Homeopathy,</p>
+          <p className="w-screen ">Bansal Market, Narsipuram Colony, Mathura, Uttar Pradesh 281006</p>
+          
+          <p className="p-5">Contact: 5857252353</p>
+          <p className="">E-mail: Jpathy@gmail.com</p>
+          
+          </div>
+      </div>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
-import cat from "../pages/cat.jpg";
+import doctor from "../assets/doctor.jpg";
+import sweet from "../assets/swee.jpg";
+import homo from "../assets/homo.jpg";
+import homo2 from "../assets/homo2.jpg";
+import homo3 from "../assets/homo3.jpg";
 
 const Home = () => {
   return (
     <>
       <div className="h-[37rem] sm:h-screen flex flex-col sm:flex-row">
         {/* Left Section */}
-        <div className="h-2/4 sm:h-full flex flex-col justify-center p-10 sm:px-20 bg-slate-500">
+        <div className="h-2/4 sm:h-full flex flex-col justify-center p-10 sm:px-20 bg-slate-500 rounded-br-3xl">
           <h1 className="text-4xl sm:text-6xl text-black">
             DR.<span className="text-[#60cf80] font-bold">Devesh</span>Jangid
           </h1>
@@ -16,45 +20,74 @@ const Home = () => {
           {/* Contact Button */}
           {/* Uncomment and adjust as needed */}
           {/* <div className="flex flex-row py-3">
-      <button className="bg-[#60cf80] text-black px-3 sm:px-5 py-1 sm:py-2 rounded-lg hover:bg-[#4f9e6f] hover:text-white text-sm sm:text-base">
-        Contact Now
-      </button>
-    </div> */}
+            <button className="bg-[#60cf80] text-black px-3 sm:px-5 py-1 sm:py-2 rounded-lg hover:bg-[#4f9e6f] hover:text-white text-sm sm:text-base">
+              Contact Now
+            </button>
+          </div> */}
         </div>
 
         {/* Right Section */}
-        <div className="h-1/2 sm:h-4/5 w-full sm:w-1/2 my-5 sm:my-20 py-10 sm:py-20 px-5 sm:px-10">
+        <div className="h-1/2 sm:h-5/6 w-full sm:w-2/5 my-5 sm:my-15 py-10 sm:py-20 px-5 sm:px-20">
           <img
-            src={cat}
+            src={doctor}
             alt="Dr. Devesh Jangid"
-            className="rounded-xl border-2 bg-purple-400 h-[200px] sm:h-full w-[250px] sm:w-full mx-auto sm:mx-20"
+            className="rounded-xl border-2 bg-purple-400 h-[200px] sm:h-full w-[250px] sm:w-full mx-auto sm:mx-44"
           />
         </div>
       </div>
 
-      <div className="h-screen ">
-        <div className="flex flex-col p-5 sm:p-10">
-          <div className="w-full text-black">
-            <h1 className="text-center text-4xl md:text-5xl font-bold">
-              Google Map Location
-            </h1>
-          </div>
-          <div className="flex justify-center">
-            <div className="w-full md:w-3/4 lg:w-1/2">
-              <div className="aspect-w-16 aspect-h-9 pt-20">
-                <iframe
-                  title="Google Map"
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d221.32420876362528!2d77.6997967338696!3d27.432271713879487!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39737702797e9af5%3A0x3cf85c1c0ac2996e!2sCMJX%2BWVV%2C%20Narsipuram%20Colony%2C%20Mathura%2C%20Uttar%20Pradesh%20281006!5e0!3m2!1sen!2sin!4v1703830076806!5m2!1sen!2sin"
-                  style={{ border: "0" }}
-                  allowFullScreen=""
-                  loading="lazy"
-                  referrerPolicy="no-referrer-when-downgrade"
-                  className="rounded-lg md:rounded-xl lg:rounded-2xl w-full h-96 md:h-80 lg:h-96"
-                ></iframe>
-              </div>
+      {/* Hero Section: Why Homeopathy */}
+      <div className="min-h-screen bg-gray-100 py-10 sm:py-32 px-5 sm:px-[15rem] mx-auto">
+        <h3 className="text-2xl sm:text-4xl text-black font-bold mb-5">
+          Why Choose Homeopathy?
+        </h3>
+        <ul className="list-none px-32">
+          <li className="text-lg sm:text-xl text-black mb-3 flex flex-row gap-5 p-5">
+            <span>
+              <img src={sweet} alt="" className="h-52 rounded-lg border-2 border-spacing-5 border-black" />
+            </span>
+            <div className="w-2/5">
+              <p className="p-10 text-center text-2xl">
+              Homeopathy relies on the principle of like cures like and highly
+              diluted substances
+            </p>
             </div>
-          </div>
-        </div>
+          </li>
+          <li className="text-lg sm:text-xl text-black mb-3 flex flex-row-reverse  gap-5 p-5">
+            <span>
+              <img src={homo} alt="" className="h-52 rounded-lg border-2 border-spacing-5 border-black" />
+            </span>
+            <div className="w-3/4">
+              <p className="p-5 text-center text-2xl">
+              Allopathy (conventional medicine) is evidence-based and undergoes
+            rigorous scientific testing.
+            </p>
+            </div>
+          </li>
+          <li className="text-lg sm:text-xl text-black mb-3 flex flex-row gap-5 p-5">
+            <span>
+              <img src={homo2} alt="" className="h-52 rounded-lg border-2 border-spacing-5 border-black" />
+            </span>
+            <div className="w-2/5">
+              <p className="p-10 text-center text-2xl">
+              Homeopathy lacks substantial scientific evidence supporting its
+            efficacy.
+            </p>
+            </div>
+          </li>
+          <li className="text-lg sm:text-xl text-black mb-3 flex flex-row-reverse  gap-5 p-5">
+            <span>
+              <img src={homo3} alt="" className="h-52 rounded-lg border-2 border-spacing-5 border-black" />
+            </span>
+            <div className="w-3/4">
+              <p className="p-5 text-center text-2xl">
+              Allopathy treatments are often backed by extensive research and
+            clinical trials.
+            </p>
+            </div>
+          </li>
+         
+        </ul>
       </div>
     </>
   );
